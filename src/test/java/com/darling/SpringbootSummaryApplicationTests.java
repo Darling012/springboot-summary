ec61eb059e97cad6dd49dc2bc7c6fc7bc5e202c6
Darling <782775618@qq.com>
2022-01-04 16:25:05 +0800

    初始化项目

---
+++ src/test/java/com/darling/SpringbootSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSummaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
